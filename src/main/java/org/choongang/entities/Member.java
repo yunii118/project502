ac23451cffc019122a04882509175cbca6a65a5b
yunii118 <lyhee118@gmail.com>
2024-01-02 09:15:17 +0900

    DDD 구조 변경

--- src/main/java/org/choongang/entities/Member.java
+++ src/main/java/org/choongang/entities/Member.java
@@ -1,16 +1,10 @@
 package org.choongang.entities;
 
 import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
 public class Member extends Base {
 
 }
