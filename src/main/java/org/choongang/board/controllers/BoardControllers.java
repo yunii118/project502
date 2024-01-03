8dc5dbc80a82ff51e7e5ff770b6e563da0f39daf
yunii118 <lyhee118@gmail.com>
2024-01-03 12:06:38 +0900

    board 테스트

---
+++ src/main/java/org/choongang/board/controllers/BoardControllers.java
@@ -0,0 +1,28 @@
+package org.choongang.board.controllers;
+
+
+import lombok.RequiredArgsConstructor;
+import org.choongang.board.entities.BoardData;
+import org.choongang.board.repositories.BoardDataRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/board")
+@RequiredArgsConstructor
+public class BoardControllers {
+
+    private final BoardDataRepository boardDataRepository;
+
+    @ResponseBody
+    @GetMapping("/test")
+    public void test(){
+        BoardData data = new BoardData();
+        data.setSubject("제목");
+        data.setContent("내용");
+
+        boardDataRepository.saveAndFlush(data);
+    }
+}
